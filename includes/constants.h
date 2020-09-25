8352d26c9622c0b401e4abd515f87ceca8acaeff
Felix <felix.phommachanh.18@cnu.edu>
2020-09-24 20:46:18 -0400

    Constants Done

--- includes/constants.h
+++ includes/constants.h
@@ -22,3 +22,12 @@ const int 			COULD_NOT_OPEN_FILE				=  SUCCESS - 2;
 const int 			NO_DATA_TO_WORK_ON				=  SUCCESS - 3;
 const int			UNIMPLEMENTED					=  SUCCESS - 4;
 const int			UNINITIALIZED					=  SUCCESS - 5;
+
+
+//some data structures
+enum SORT_ORDER { PROCESS_NUMBER, CPU_TIME, START_TIME };
+struct process{	int process_number=UNINITIALIZED;
+				int cpu_time=UNINITIALIZED;
+				int start_time=UNINITIALIZED;};
+				
+#endif /* CONSTANTS_H_ */
--- src/proj_refactor_file.cpp
+++ src/proj_refactor_file.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : proj_refactor_file
-// Author      : 
+// Author      : Felix Phommachanh
 // Version     :
 // Copyright   : Steal this code!
 // Description : learn how to refactor files into a sensible directory structure
@@ -8,30 +8,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "../includes/constants.h"
 
 using namespace std;
 
-//default files to read and write data
-const std::string SOURCE_FILE 	= "source_file.txt";
-const std::string RESULTS_FILE 	= "results_file.txt";
-
-//default return types
-const int 			EMPTY			 				=  0;
-const int 			SUCCESS			 				=  0;
-const int 			FAIL							=  SUCCESS - 1;
-const int 			COULD_NOT_OPEN_FILE				=  SUCCESS - 2;
-const int 			NO_DATA_TO_WORK_ON				=  SUCCESS - 3;
-const int			UNIMPLEMENTED					=  SUCCESS - 4;
-const int			UNINITIALIZED					=  SUCCESS - 5;
-
-//some data structures
-enum SORT_ORDER { PROCESS_NUMBER, CPU_TIME, START_TIME };
-struct process{	int process_number=UNINITIALIZED;
-				int cpu_time=UNINITIALIZED;
-				int start_time=UNINITIALIZED;};
-
-//holds all the processes read in
-vector<process> myProcesses;
 
 //attempt to open file 'filename' and read in all data
 //returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE

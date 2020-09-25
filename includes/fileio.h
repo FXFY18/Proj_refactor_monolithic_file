0cd5f2866f9c35d9e47897b054043d25baf3da16
Felix <felix.phommachanh.18@cnu.edu>
2020-09-24 21:14:26 -0400

    File IO

--- includes/fileio.h
+++ includes/fileio.h
@@ -1,4 +1,15 @@
+/*
+* fileio.h
+* Version: 9/24/2020
+* Creator: Felix Phommachanh
+*
+*/
 
+#ifndef FILEIO_H_
+#define FILEIO_H_
+#include <string>
 
 int loadData(const std::string filename, vector<process> &myProcesses);
 int saveData(const std::string filename, vector<process> &myProcesses);
+
+#endif /* FILEIO_H_ */
--- src/proj_refactor_file.cpp
+++ src/proj_refactor_file.cpp
@@ -9,21 +9,10 @@
 #include <string>
 #include <vector>
 #include "../includes/constants.h"
+#include "../includes/fileio.h"
+
 
 using namespace std;
-
-
-//attempt to open file 'filename' and read in all data
-//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
-int loadData(const std::string filename, vector<process> &myProcesses){
-	return UNIMPLEMENTED;
-}
-
-//attempt to create or open file 'filename' to write all data to
-//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
-int saveData(const std::string filename, vector<process> &myProcesses){
-	return UNIMPLEMENTED;
-}
 
 //sorts vector inplace based on mySortOrder (inplace means the vector is modified)
 //returns nothing

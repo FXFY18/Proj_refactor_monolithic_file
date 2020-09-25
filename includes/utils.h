2b7ea31687a4abeae8950fd6988f3512c57d3c60
Felix <felix.phommachanh.18@cnu.edu>
2020-09-24 21:24:11 -0400

    Utils done

---
+++ includes/utils.h
@@ -0,0 +1,16 @@
+/*
+* utils.h
+* version: 9/24/2020
+* Created: Felix Phommachanh
+*
+*/
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+void sortData(const SORT_ORDER &mySortOrder,vector<process> &myProcesses);
+process getNext(vector<process> &myProcesses);
+int getSize(vector<process> &myProcesses);
+int handleMissingData(vector<process> &myProcesses);
+
+#endif /* UTILS_H_ */

1e0aab614e082251e665779ca5940afac6b17d71
Felix <felix.phommachanh.18@cnu.edu>
2020-09-26 12:12:35 -0400

    Problem solved now.

--- includes/fileio.h
+++ includes/fileio.h
@@ -12,9 +12,8 @@
 #include "constants.h"
 #include <vector>
 
-//std::vector<process>, does not work at all. 
 
-int loadData(const std::string filename, vector<process> &myProcesses);
-int saveData(const std::string filename, vector<process> &myProcesses);
+int loadData(const std::string filename, std::vector<process> &myProcesses);
+int saveData(const std::string filename, std::vector<process> &myProcesses);
 
 #endif /* FILEIO_H_ */
